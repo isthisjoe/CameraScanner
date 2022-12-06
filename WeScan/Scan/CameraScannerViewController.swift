45412e445bcb09475b8d10b228f1275ab500d317
Joe Nguyen <isthisjoe@gmail.com>
2022-12-06 11:41:03 -0500

    Update bg color to black

--- WeScan/Scan/CameraScannerViewController.swift
+++ WeScan/Scan/CameraScannerViewController.swift
@@ -99,7 +99,7 @@ public final class CameraScannerViewController: UIViewController {
     }
     
     private func setupView() {
-        view.backgroundColor = .darkGray
+        view.backgroundColor = .black
         view.layer.addSublayer(videoPreviewLayer)
         quadView.translatesAutoresizingMaskIntoConstraints = false
         quadView.editable = false
